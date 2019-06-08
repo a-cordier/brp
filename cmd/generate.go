ab047fef6f8aeebe2d1daa17c7905b74310e9e77
Antoine Cordier <com.acordier@gmail.com>
2019-06-08 14:52:37 +0200

    doc: update generate command description

--- cmd/generate.go
+++ cmd/generate.go
@@ -41,9 +41,13 @@ var generateCmd = &cobra.Command{
 	Long: `
 	Generate a binary resources source file from a resource folder:
 
-		- resources will be accessible from the given ns, defaulting to camel cased folder name is missing
-		- resources will be written to the file given by the -o flag, defaulting to stdout if missing
-		- resources will be written for the -l flag language, defaulting to cpp if missing
+	  - resources will be accessible from the given ns, defaulting to camel cased folder name is missing
+	  - resources will be written to the file given by the -o flag, defaulting to stdout if missing
+      - resources will be written for the -l flag language, defaulting to cpp if missing
+
+	Supported languages:
+	  - cpp (--lang cpp)
+	  - golang (--lang go)
 	`,
 	Use: "generate [FOLDER]",
 	Args: func(cmd *cobra.Command, args []string) error {
